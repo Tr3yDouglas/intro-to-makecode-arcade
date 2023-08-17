448bc89ab189d87db3c7f23733187d1720b6bebe
Tr3yDouglas <142516234+Tr3yDouglas@users.noreply.github.com>
2023-08-17 10:56:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    mySprite.startEffect(effects.spray, randint(0, 600))
+})
+game.splash("These blocks are in your workspace!")
+game.onUpdateInterval(500, function () {
+	
+})
+game.onUpdateInterval(500, function () {
+	
+})
+game.onUpdateInterval(500, function () {
+	
+})
